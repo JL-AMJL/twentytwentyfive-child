91dd919a8b6b809dce28e825a275b1edad054dfa
JL-AMJL <62677437+JL-AMJL@users.noreply.github.com>
2025-04-03 19:15:40 +0200

    fixed bug in functions.php

--- functions.php
+++ functions.php
@@ -14,12 +14,3 @@ add_action( 'wp_enqueue_scripts', function() {
         true
     );
 }, 20 );
-
-add_filter( 'wp_theme_json_data_theme', function( $theme_json ) {
-    // Nur eingreifen, wenn Child-Theme aktiv ist
-    if ( get_template() === 'twentytwentyfive' && is_child_theme() ) {
-        // Entferne Style Variations des Parent-Themes
-        remove_theme_support( 'theme.json' );
-    }
-    return $theme_json;
-}, 20 );
