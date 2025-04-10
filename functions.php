9572ca95f361ba55c30b1fe814bc30f27f8ddbd1
JL-AMJL <62677437+JL-AMJL@users.noreply.github.com>
2025-04-10 19:03:36 +0200

    inherit color palett from parent theme

--- functions.php
+++ functions.php
@@ -30,3 +30,35 @@ add_action( 'wp_enqueue_scripts', function() {
         true
     );
 }, 20 );
+
+// 4. Inherit parent theme.json color settings (palette, gradients, duotone)
+add_filter( 'theme_json_data_theme', function( $theme_json ) {
+    if ( is_child_theme() ) {
+        $parent = wp_get_theme()->parent();
+        $parent_json_path = get_theme_root() . '/' . $parent->get_stylesheet() . '/theme.json';
+
+        if ( file_exists( $parent_json_path ) ) {
+            $parent_json = json_decode( file_get_contents( $parent_json_path ), true );
+            $child_data  = $theme_json->get_data();
+
+            // Inherit Color Palette
+            if ( isset( $parent_json['settings']['color']['palette'] ) ) {
+                $child_data['settings']['color']['palette'] = $parent_json['settings']['color']['palette'];
+            }
+
+            // Inherit Gradients
+            if ( isset( $parent_json['settings']['color']['gradients'] ) ) {
+                $child_data['settings']['color']['gradients'] = $parent_json['settings']['color']['gradients'];
+            }
+
+            // Inherit Duotone
+            if ( isset( $parent_json['settings']['color']['duotone'] ) ) {
+                $child_data['settings']['color']['duotone'] = $parent_json['settings']['color']['duotone'];
+            }
+
+            $theme_json->update( $child_data );
+        }
+    }
+
+    return $theme_json;
+} );
